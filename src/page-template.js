988ae60f1c7cfd23d739e1391810f2bce7aab6ce
bchen41 <bettychen41@outlook.com>
2021-12-02 22:53:17 -0500

    refactored code, added a tag for github profile

--- src/page-template.js
+++ src/page-template.js
@@ -31,7 +31,7 @@ const generateTeam = (team) => {
             <ul class="list-group">
                 <li class="list-group-item">ID: ${engineer.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${engineer.getGithub()}</li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
             </ul>
         </div>
     </div>
@@ -50,7 +50,7 @@ const generateTeam = (team) => {
             <ul class="list-group">
                 <li class="list-group-item">ID: ${intern.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${intern.getSchool()}</li>
+                <li class="list-group-item">School: ${intern.getSchool()}</li>
             </ul>
         </div>
     </div>
